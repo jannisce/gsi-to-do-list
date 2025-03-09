61ec21cae82379a4f13add376fed3c58b42fc708
Javier Chavez <77762186+jannisce@users.noreply.github.com>
2025-03-09 17:15:13 -0600

    feat: add error handling for empty task input in AddTask component

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 
 const AddTask = ({ handleCreate }) => {
   const [newTodo, setNewTodo] = useState('')
+  const [error, setError] = useState(false)
 
   const onChange = (e) => {
     setNewTodo(e.target.value)
+    if (error) {
+      setError('')
+    }
   }
 
   const onAddTask = () => {
     if (newTodo) {
       handleCreate(newTodo)
       setNewTodo('') // Reset the input after adding the task
+    } else {
+      setError('Please enter a title for a new task')
     }
   }
 
@@ -22,22 +28,31 @@ const AddTask = ({ handleCreate }) => {
   }
 
   return (
-    <div className='flex items-center space-x-4 mb-6'>
-      <input
-        type='text'
-        value={newTodo}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        placeholder='Add a new task'
-        className='w-3/4 p-3 border-2 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-      />
-      <button
-        onClick={onAddTask}
-        className='w-1/3 p-3 border-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500'
-      >
-        Add Task
-      </button>
-    </div>
+    <>
+      <div className='flex items-center space-x-4 mb-2'>
+        <input
+          type='text'
+          value={newTodo}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          placeholder='Add a new task'
+          className='w-3/4 p-3 border-2 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+        />
+        <button
+          onClick={onAddTask}
+          className='w-1/3 p-3 border-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500'
+        >
+          Add Task
+        </button>
+      </div>
+      <div className='flex items-center'>
+        {error && (
+          <div className='text-center text-red-600'>
+            <p>{error}</p>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
